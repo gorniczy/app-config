6620fb6470e9ff94b45977b6947154602e14e5a9
gorniczy <denysgornichar@gmail.com>
2019-03-25 21:25:06 +0100

    add module to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,17 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'App Config'
     })
-  ]
+  ],
+  module: {
+    rules: [{
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }]
+  }
 }
