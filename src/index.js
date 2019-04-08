e677c39d14d9031fe64a0c068335add5d9c17780
gorniczy <denysgornichar@gmail.com>
2019-04-08 22:33:44 +0200

    add file-loader and import images to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import Script from './script'
 import './main.scss'
+import Bijou from './img/bijou.jpg'
+import Eggnogg from './img/eggnogg.jpg'
+import Mojito from './img/mojito.jpg'
+import Negroni from './img/negroni.jpg'
+import Rattlesnake from './img/rattlesnake.jpg'
+import Zinger from './img/zinger.jpg'
 
 Script()
