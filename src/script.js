7e543f25b3dc0f90d0bf904e06aba5b5d90771ca
gorniczy <denysgornichar@gmail.com>
2019-04-10 21:26:42 +0200

    add images through the script

--- src/script.js
+++ src/script.js
@@ -1,3 +1,15 @@
-export default function () {
-  console.log('imported!');
+import Bijou from './img/bijou.jpg'
+import Eggnogg from './img/eggnogg.jpg'
+import Mojito from './img/mojito.jpg'
+import Negroni from './img/negroni.jpg'
+import Rattlesnake from './img/rattlesnake.jpg'
+import Zinger from './img/zinger.jpg'
+
+const images = [Bijou, Eggnogg, Mojito, Negroni, Rattlesnake, Zinger]
+const node = document.querySelectorAll('.drink');
+
+export const insertImages = () => {
+  for (let i = 0; i < node.length; i++) {
+    node[i].innerHTML = `<img src="${images[i]}" alt="drink">`;
+  }
 }
